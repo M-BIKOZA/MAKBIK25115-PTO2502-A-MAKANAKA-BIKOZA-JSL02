47ceb1066f3655ba7325828be2cadfe08b83cce9
M-Bikoza <bikoza2005@gmail.com>
2025-06-20 16:42:06 +0200

    added a javascript file

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 //for loop to manage multiple tasks
-for (let i=0;i <2;++) {
+for (let i=0;i <2;++)
+     {
     //declaring variables to store input
+    let task = prompt ("Enter task"+ (i+1)+"tittle:");
+    let description= prompt (Entre task"+ (i+1) +" description:");
+        let taskStatus=""
 
 }
