8cd36b9660f2b7e709c60c73d91db45d06fc4058
M-Bikoza <bikoza2005@gmail.com>
2025-06-22 00:18:04 +0200

    added task 2 inputs

--- script.js
+++ script.js
@@ -11,5 +11,30 @@ while(true) {
         task1Status === "todo"||
         task1Status === "doing"||
         task1Status === "done"||
-    )
+    ) {
+        break;
+    } else {
+        alert("Invalid status. Please enter 'todo', 'doing', 'done'.");
+    }
 }
+
+//Task 2 inputs
+let task2Title = prompt("Enter task 2 title:");
+let task2Description = prompt("Enter task 2 description");
+let task2Status ="",
+
+while (true) {
+    task2Status = prompt (
+        "Enter task 2 status (todo, doing, done):"
+    ).toLowerCase ();
+    if (
+        task2Status === "todo"||
+        task2Status === "doing"||
+        task2Status === "done"||
+    ) {
+        break;
+    } else {
+        alert("Invalid status. Please enter 'todo', 'doing', 'done',");
+    }
+}
+
