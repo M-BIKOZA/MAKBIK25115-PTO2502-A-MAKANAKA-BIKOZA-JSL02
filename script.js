2d255232f990fdc370971784d95b0eaa74020c03
M-Bikoza <bikoza2005@gmail.com>
2025-06-21 18:31:34 +0200

    added a script.js link in my html

--- script.js
+++ script.js
@@ -5,5 +5,20 @@ for (let i=0;i <2;++)
     let task = prompt ("Enter task"+ (i+1)+"tittle:");
     let description= prompt (Entre task"+ (i+1) +" description:");
         let taskStatus=""
+        
+        if (
+            taskStatus==="todo"||
+            taskStatus==="doing"||
+            taskStatus==="done"
+        ) {
+            if (taskStatus==="done") {
+                console.log("Tittle:" + task +", status:" + taskStatus);
+            }
+            else{
+                console.log("No task completed, let's get to work!");
+            }
+        }
 
-}
+} while (taskedStatus !=="todo"
+    &&taskStatus!=="doing"
+    &&taskStatus!=="done");
